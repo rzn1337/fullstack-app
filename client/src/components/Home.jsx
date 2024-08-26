d24132bc5beb1d4198fee654b893c435d970c0ba
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-08-26 21:46:33 +0500

    fix home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header/Header";
 
 function Home() {
-    return (
+    const location = useLocation();
+    const hideHeader = location.pathname === "/canvas";
+    console.log(hideHeader);
+    /* return hideHeader ? (
+        <div>
+            <Outlet />
+        </div>
+    ) : (
         <div>
             <Header />
+            <Outlet />
+        </div>
+    ); */
+
+    // short circuit eval
+    return (
+        <div>
+            {!hideHeader && <Header />}
             <Outlet />
         </div>
     );
