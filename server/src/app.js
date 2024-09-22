8a57758cc2ac3c2918976cb2829dc8330d2819cb
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-09-22 14:11:28 +0500

    fix payload size

--- server/src/app.js
+++ server/src/app.js
@@ -40,8 +40,8 @@ app.use(
     })
 );
 
-app.use(express.json({ limit: "20kb" }));
-app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
